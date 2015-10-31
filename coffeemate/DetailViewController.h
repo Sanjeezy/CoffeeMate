f67ba8d38929ec9a409936db15567d64dd8026d9
Sanjeezy <sanjayt@sanjays-macbook-pro.local>
2015-10-31 10:46:58 -0700

    Initial Commit

---
+++ coffeemate/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  coffeemate
+//
+//  Created by Sanjay Tamizharasu on 10/31/15.
+//  Copyright © 2015 SanjayTamizharasu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ coffeemate/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  coffeemate
+//
+//  Created by Sanjay Tamizharasu on 10/31/15.
+//  Copyright © 2015 SanjayTamizharasu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
